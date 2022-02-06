77b4a5c74a65e9ff03eb8787808fe75a54d9987d
anupam41 <anupamsamanta41@gmail.com>
2022-02-06 13:47:13 +0530

    YASP: Move theming to YASP

--- src/com/yasp/settings/fragments/Customisation.java
+++ src/com/yasp/settings/fragments/Customisation.java
@@ -30,6 +30,7 @@ import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.UserHandle;
+import com.android.settings.development.OverlayCategoryPreferenceController;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import androidx.annotation.VisibleForTesting;
@@ -40,16 +41,20 @@ import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 import android.text.TextUtils;
+import androidx.fragment.app.Fragment;
 import android.util.Log;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 import android.view.WindowManagerGlobal;
 import android.view.IWindowManager;
 import android.widget.Toast;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import com.android.settings.dashboard.DashboardFragment;
 import java.util.Locale;
 import android.text.TextUtils;
+
 import android.view.View;
 
 import com.android.settings.R;
@@ -61,7 +66,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class Customisation extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+public class Customisation extends DashboardFragment implements OnPreferenceChangeListener {
 
     private static final String TAG = "Customisation";
 
@@ -71,13 +76,16 @@ public class Customisation extends SettingsPreferenceFragment implements OnPrefe
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.customisation);
-
         mContext = getActivity();
 
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen screen = getPreferenceScreen();
 
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+    	return R.xml.customisation;
     }
 
     @Override
@@ -88,6 +96,26 @@ public class Customisation extends SettingsPreferenceFragment implements OnPrefe
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, Lifecycle lifecycle, Fragment fragment) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.font"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.icon_pack"));
+        return controllers;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
     }
 
     @Override
