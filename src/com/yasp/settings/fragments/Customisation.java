03b1806330c33a319e610620d02aa8be04f43003
SagarMakhar <sagarmakhar@gmail.com>
2022-02-18 20:13:09 +0000

    YASP: Bring back icon shapes, icon packs, fonts, signal icon customization
    Change-Id: I0a80a90faa83d94ec8652fc48faee8e1ae710f09
    
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/yasp/settings/fragments/Customisation.java
+++ src/com/yasp/settings/fragments/Customisation.java
@@ -30,7 +30,6 @@ import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.UserHandle;
-import com.android.settings.development.OverlayCategoryPreferenceController;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import androidx.annotation.VisibleForTesting;
@@ -41,20 +40,16 @@ import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 import android.text.TextUtils;
-import androidx.fragment.app.Fragment;
 import android.util.Log;
-import com.android.settingslib.core.AbstractPreferenceController;
-import com.android.settingslib.core.lifecycle.Lifecycle;
 import android.view.WindowManagerGlobal;
 import android.view.IWindowManager;
 import android.widget.Toast;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import com.android.settings.dashboard.DashboardFragment;
+
 import java.util.Locale;
 import android.text.TextUtils;
-
 import android.view.View;
 
 import com.android.settings.R;
@@ -66,7 +61,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class Customisation extends DashboardFragment implements OnPreferenceChangeListener {
+public class Customisation extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String TAG = "Customisation";
 
@@ -76,16 +71,13 @@ public class Customisation extends DashboardFragment implements OnPreferenceChan
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        addPreferencesFromResource(R.xml.customisation);
+
         mContext = getActivity();
 
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen screen = getPreferenceScreen();
 
-    }
-
-    @Override
-    protected int getPreferenceScreenResId() {
-    	return R.xml.customisation;
     }
 
     @Override
@@ -96,26 +88,6 @@ public class Customisation extends DashboardFragment implements OnPreferenceChan
     @Override
     public void onResume() {
         super.onResume();
-    }
-
-    @Override
-    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
-        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
-    }
-
-    private static List<AbstractPreferenceController> buildPreferenceControllers(
-            Context context, Lifecycle lifecycle, Fragment fragment) {
-        final List<AbstractPreferenceController> controllers = new ArrayList<>();
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.font"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.icon_pack"));
-        return controllers;
-    }
-
-    @Override
-    protected String getLogTag() {
-        return TAG;
     }
 
     @Override
